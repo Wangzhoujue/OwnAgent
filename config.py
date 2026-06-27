eac388fecda596fc7a34d54d44147703cdf95d65
Wangzhoujue <Baozaoderenji@gmail.com>
2026-06-27 23:32:12 +0800

    重置config

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import os
 class Config:
     def __init__(self):
         # AI模型配置
-        self.api_base_url = "http://172.16.166.2:8900/v1"
+        self.api_base_url = "The_API_base_url"
         self.api_key = "Your_api_key"
-        self.ai_model = "dsv4"
+        self.ai_model = "Modelname"
        
 config = Config()
