4f9fafda2fd5aaec1de8b6b4f84f601876094c5c
Wangzhoujue <Baozaoderenji@gmail.com>
2026-06-27 23:01:07 +0800

    移除 gradio 依赖

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import os
 class Config:
     def __init__(self):
         # AI模型配置
-        self.api_base_url =  ""
-        self.api_key = ""
+        self.api_base_url =  "http://172.16.166.2:8900/v1"
+        self.api_key = " "
         self.ai_model = ""
        
 config = Config()
